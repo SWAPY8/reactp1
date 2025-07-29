f8e336ff9f1c4233e81537ccf3a07731cfcd0d6d
Swapnil Gandhale <swapnilgandhale08@gmail.com>
2025-07-29 11:39:49 +0530

    p1 done

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,35 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
+
+  const addvalue = () => {
+    if(count > 19){
+      count = 20;
+    }
+    setCount(count + 1);
+  };
+  const removevalue = () => {
+    if(count <= 0){
+      count = 0;
+    }
+    setCount(count - 1);
+  };
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
       <h1>Vite + React</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+        <p>  Count is {count} </p>
+        <button onClick={addvalue}>
+          addvalue
+        </button>
+        <br /><br />
+        <button onClick={removevalue}>
+          removevalue
         </button>
         <p>
           Edit <code>src/App.jsx</code> and save to test HMR
@@ -29,7 +39,7 @@ function App() {
         Click on the Vite and React logos to learn more
       </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
