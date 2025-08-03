4db07786988b4922f6effe2745fef32b532091b9
SWAPY8 <swapnilofficial08@gmail.com>
2025-08-03 10:49:09 +0530

    done with feching eg

--- src/main.jsx
+++ src/main.jsx
@@ -5,7 +5,6 @@ import App from './App.jsx';
 import './index.css';
 import { AuthProvider } from './AuthContext.jsx'; // Import the AuthProvider
 
-<<<<<<< HEAD
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <AuthProvider> {/* Wrap your App with the AuthProvider */}
@@ -13,11 +12,3 @@ ReactDOM.createRoot(document.getElementById('root')).render(
     </AuthProvider>
   </React.StrictMode>,
 );
-=======
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
- 
->>>>>>> f8e336ff9f1c4233e81537ccf3a07731cfcd0d6d
